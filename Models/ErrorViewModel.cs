528734187c4e0ee4adcfe6a2a8ed24deef3a9635
Luan Ba Nguyen <luan0010@edu.ucl.dk>
2020-11-25 11:38:08 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Makers_of_Denmark.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
