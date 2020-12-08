f4494a94e48b70290e1552e7cca6e8297c335b51
Luan Ba Nguyen <luan0010@edu.ucl.dk>
2020-12-08 12:40:32 +0100

    Feed og api forsøg

---
+++ Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Makers_of_Denmark.Models
+{
+    public class Address
+    {
+        public string  street { get; set; }
+        public string postCode { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+    }
+}
