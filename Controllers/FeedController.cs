3238c2ee96c3ce33fd32c3d6259bcba84b93da4e
Luan Ba Nguyen <luan0010@edu.ucl.dk>
2020-12-16 11:20:08 +0100

    feed css

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -17,20 +17,14 @@ namespace Makers_of_Denmark.Controllers
         private Uri BaseEndPoint { get; set; }
         public FeedController() 
         {
-            // Set the port to whatever the API port is once you've started it once. Shouldn't change on restarts.
             BaseEndPoint = new Uri("https://makersofdenmark.azurewebsites.net");
             _httpClient = new HttpClient();
         }
         public async Task<IActionResult> IndexAsync()
         {
-            // use HTTP client to read data from API. Move on once the headers have been read. Errors are caught slightly quicker this way.
             var response = await _httpClient.GetAsync(BaseEndPoint + "/MakerSpace", HttpCompletionOption.ResponseHeadersRead);
-            // Make sure that we got a success status code in the headers. Returns an exception (and 500 status code) if not successful
             response.EnsureSuccessStatusCode();
-            // Turn the response body into a string
             var data = await response.Content.ReadAsStringAsync();
-            // Treat the response body string as JSON, and deserialize it into a list of flights
-
             MakerspacesModel makerspacesModel = JsonConvert.DeserializeObject<MakerspacesModel>(data);
 
             return View(makerspacesModel.makerSpaces);
