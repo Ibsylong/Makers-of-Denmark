5c7fda37be9eecc8cfd743184cf84a70e4eae408
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2020-12-17 19:11:49 +0100

    added event and tool models

---
+++ Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Makers_of_Denmark.Models
+{
+    public class Event
+    {
+        public string id { get; set; }
+        public string makerSpaceId { get; set; }
+        public string address { get; set; }
+        public string title { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string description { get; set; }
+        public User[] participants { get; set; }
+        public string badge { get; set; }
+    }
+}
---
+++ Models/Tool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Makers_of_Denmark.Models
+{
+    public class Tool
+    {
+        public string id { get; set; }
+        public string makerSpaceId { get; set; }
+        public string make { get; set; }
+        public string model { get; set; }
+
+    }
+}
