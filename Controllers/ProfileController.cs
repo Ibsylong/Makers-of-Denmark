657604daf2de39787cf3ee425cca5207d5962265
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2020-12-17 19:08:40 +0100

    profile form working

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,16 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
-
+using Makers_of_Denmark.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 namespace Makers_of_Denmark.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _httpClient;
+        private Uri BaseEndPoint { get; set; }
+        private string endpoint = "user";
+
+        public ProfileController()
         {
-            return View();
+            BaseEndPoint = new Uri("https://makersofdenmark.azurewebsites.net");
+            _httpClient = new HttpClient();
+        }
+
+        [Route("[controller]/{id}")]
+        public async Task<IActionResult> IndexAsync(string id)
+        {
+            var response = await _httpClient.GetAsync($"{BaseEndPoint}/{endpoint}/{id}", HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsStringAsync();
+            User user = JsonConvert.DeserializeObject<User>(data);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(string id, User user)
+        {
+            string json = JsonConvert.SerializeObject(user);
+            var response = await _httpClient.PutAsync($"{BaseEndPoint}/{endpoint}/{id}", new StringContent(json, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+
+            return RedirectToAction(id, "Profile");
         }
     }
 }
