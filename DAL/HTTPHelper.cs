97b5e243ef1fb184984c115a553184a1957dd700
Jesper Møller Jensen <jjensenmoeller@gmail.com>
2020-12-19 11:17:55 +0100

    Added HTTPHelper class

---
+++ DAL/HTTPHelper.cs
@@ -0,0 +1,43 @@
+﻿using Makers_of_Denmark.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Makers_of_Denmark.DAL
+{
+    public class HTTPHelper
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly Uri baseEndPoint = new Uri("https://makersofdenmark.azurewebsites.net");
+
+        public User GetUser()
+        {
+            User user = new User();
+            return user;
+        }
+
+         public async Task<T> GetWithID<T>(string endpoint, string id)
+        {
+            var response = await _httpClient.GetAsync($"{baseEndPoint}/{endpoint}/{id}", HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsStringAsync();
+            T result = JsonConvert.DeserializeObject<T>(data);
+
+            return result;
+        }
+
+        public async Task<Task> PostWithID(string endpoint, string id, object objectData)
+        {
+            string json = JsonConvert.SerializeObject(objectData);
+            var response = await _httpClient.PutAsync($"{baseEndPoint}/{endpoint}/{id}", new StringContent(json, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+
+            return Task.CompletedTask;
+        }
+    }
+}
